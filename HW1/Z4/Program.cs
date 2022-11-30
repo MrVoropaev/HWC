a369bde231017c1f0b83d7cd534ee436716314be
MrVoropaev <mrvoropaev@gmail.com>
2022-11-30 09:18:23 +0300

    Fourth question

---
+++ HW1/Z4/Program.cs
@@ -0,0 +1,17 @@
+﻿// На вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1; 
+
+while (i < n)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
+
+
