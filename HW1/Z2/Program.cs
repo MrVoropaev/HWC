eaeba4a18227281916aece2d506800e51f496b49
MrVoropaev <mrvoropaev@gmail.com>
2022-11-30 08:39:25 +0300

    Second question

---
+++ HW1/Z2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int maximalnumber = number1;
+
+if (number2>maximalnumber)
+{
+    maximalnumber = number2;
+}
+
+if (number3>maximalnumber)
+{
+    maximalnumber = number3;
+}
+
+Console.WriteLine("Максимальное число:" + maximalnumber);
+
+
