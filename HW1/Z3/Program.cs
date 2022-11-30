2a0f5e169b6891451d128e48c56e3e611bcfe244
MrVoropaev <mrvoropaev@gmail.com>
2022-11-30 08:46:50 +0300

    Third question

---
+++ HW1/Z3/Program.cs
@@ -0,0 +1,14 @@
+﻿// На вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+else
+{
+    Console.WriteLine($"{number} - не четное число");
+}
+
