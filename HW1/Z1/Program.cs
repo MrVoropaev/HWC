6bccdc83445d0a083144055b558ef5ba0adf1800
MrVoropaev <mrvoropaev@gmail.com>
2022-11-30 08:19:44 +0300

    First question

---
+++ HW1/Z1/Program.cs
@@ -0,0 +1,16 @@
+﻿//На вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} больше {number2}");
+}
+else
+{
+    Console.WriteLine($"{number2} больше {number1}");
+}
