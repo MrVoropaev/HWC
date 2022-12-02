37e488726860b6ebffcd117394739f10bc5e9dde
MrVoropaev <mrvoropaev@gmail.com>
2022-12-02 10:27:28 +0300

    HW2 Z3

--- HW2/Z1/Program.cs
+++ HW2/Z1/Program.cs
@@ -1,5 +1,13 @@
 ﻿//Принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 
-Console.WriteLine("Введите трехзначное число: ");
-string input = Console.ReadLine();
-Console.WriteLine("{0}->{1}",input, input[1]);
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length == 3)
+{
+  Console.WriteLine("Вторая цифра:  " + numberText[1]);
+}
+else 
+{ 
+    Console.WriteLine("Число не трехзначное");
+}
