ee181783c72364c38fe0cefd0565c20dac6cd017
MrVoropaev <mrvoropaev@gmail.com>
2022-12-02 09:59:40 +0300

    HW2 Z3

---
+++ HW2/Z3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Write("Введи номер дня: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void DayWeekend(int day)
+{
+    if (day == 6 || day ==7 )
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else if (day <1 || day > 7)
+    {
+        Console.WriteLine("Это не день");
+    }
+    else
+    {
+        Console.WriteLine("Это будни");
+    }
+}
+
+DayWeekend(day);
+
+
