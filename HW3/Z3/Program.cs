450e0eee9d3ee1546d360399f36f0236c3f2548e
MrVoropaev <mrvoropaev@gmail.com>
2022-12-06 08:48:27 +0300

    HW3 Z3

---
+++ HW3/Z3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+/*Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= number)
+{
+    if (i != 1)
+    {
+        Console.Write(i*i*i + " ");
+    }
+    i++;
+} */
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadNumber("Введи число: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i} ");
+}
+
