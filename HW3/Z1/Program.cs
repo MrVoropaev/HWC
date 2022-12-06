fbd313268d85129e6c06d23853bdb7f4b150b64d
MrVoropaev <mrvoropaev@gmail.com>
2022-12-06 07:41:13 +0300

    HW3 Z1

---
+++ HW3/Z1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length == 5)
+{
+    if (numberText[0] == numberText[4] && numberText[1] == numberText[3])
+    {
+        Console.WriteLine("Это полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не полиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Это не пятизначное число");
+}
