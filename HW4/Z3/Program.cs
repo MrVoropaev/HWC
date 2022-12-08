92847fd23404b3529df2f09f5db96000c4ab6f67
MrVoropaev <mrvoropaev@gmail.com>
2022-12-08 16:50:10 +0300

    HW4

--- HW4/Z3/Program.cs
+++ HW4/Z3/Program.cs
@@ -1,18 +1,27 @@
 ﻿// Задаёт массив из 8 элементов и выводит их на экран
+using System;  
+public class Exercise1  
+{  
+    public static void Main()  
+{  
+    int[] arr = new int[10]; 
+    int i;  
 
-int lenArray = ReadInt("Введите число: ");
-
-int[] randomArray = new int[lenArray];
-for (int i = 0; i < randomArray.Length; i++)
-{
-    randomArray[i] = new lenArray();
-    Console.Write(randomArray[i] + " ");
+    Console.Write("Input 10 elements in the array :");  
+    for(i=0; i<8; i++)  
+    {  
+	    Console.WriteLine();
+	    arr[i] = Convert.ToInt32(Console.ReadLine());  		
+    }  
+  
+    Console.Write("\nElements in array are: ");  
+    for(i=0; i<8; i++)  
+    {  
+        Console.WriteLine("[{0}]", string.Join(", ", arr[i])); 
+    } 	
+  }
 }
 
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+
 
 
