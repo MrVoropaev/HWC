3e78df4efdba6e7fe7b62879ad61be25940df9a1
MrVoropaev <mrvoropaev@gmail.com>
2022-12-20 07:51:38 +0300

    HW7

---
+++ HW7/Z1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GenDoubleArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine()??"");
+    Console.Write("Введите число строк массива n: ");
+    int n = int.Parse(Console.ReadLine()??"");
+    double[,] arr = new double[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = Math.Round(rand.Next(-10, 11) * rand.NextDouble(), 1);
+        }
+
+    }
+    return arr;
+}
+
+void PrintDoubleArray(double[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+double[,] array = GenDoubleArray();
+PrintDoubleArray(array);
---
+++ HW7/Z2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+double[,] matrix = new double[4, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write("{0} ", matrix[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите позиции элемента через запятую: ");
+int[] position = Console.ReadLine().Split(',').Select(Int32.Parse).ToArray();
+if (position[0] > matrix.GetLength(0) - 1 || position[1] > matrix.GetLength(1) - 1)
+{
+    Console.WriteLine("Такого элемента нет.");
+}
+else
+{
+    Console.WriteLine(matrix[position[0], position[1]]);
+}
